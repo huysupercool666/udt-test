b1e3c0a4869bf95c20b1ac788bcc89200661aae6
huyTranDu <huytd3011001@gmail.com>
2024-12-25 00:27:25 +0700

    WIP

--- Calculator/src/client/index.jsx
+++ Calculator/src/client/index.jsx
@@ -1,7 +1,7 @@
 import { hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-
+import "../styles/app.scss"
 const domNode = document.getElementById('root')
 
 hydrateRoot(
