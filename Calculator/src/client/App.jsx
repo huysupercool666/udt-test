11a084f454c9ab25976f72cc5cb7f3dc53e67a98
huyTranDu <huytd3011001@gmail.com>
2024-12-24 19:24:42 +0700

    WIP

--- Calculator/src/client/App.jsx
+++ Calculator/src/client/App.jsx
@@ -1,7 +1,12 @@
-import { Route, Routes } from 'react-router-dom'
-
+import { Route, Routes } from "react-router-dom";
 const App = () => {
-  return <h1>Hello World</h1>
-}
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
